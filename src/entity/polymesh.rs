248f459be7638a0b0e6889c0b8c4e9ea3af5cf57
Arvin Singh Kushwaha <askushwa@ncsu.edu>
2022-04-27 13:41:36 -0400

    Implement coplanarity checks

--- src/entity/polymesh.rs
+++ src/entity/polymesh.rs
@@ -68,11 +68,36 @@ pub struct Polygon {
     pub normal: Vec3,
 }
 
+impl Polygon {
+    /// Creates a new `Polygon` with the given vertices.
+    /// The normal of the polygon will be calculated from the vertices.
+    /// The normal will be normalized. If the vertices are not coplanar,
+    /// no polygon will be created. A polygon also must have at least 3
+    /// vertices.
+    ///
+    /// # Arguments
+    /// * `vertices` - The list of vertices that make up the polygon.
+    /// * `vertex_list` - The list of vertices that make up the mesh.
+    ///
+    /// # Returns
+    /// An `Option<Polygon>` containing `Some(Polygon)` if there are at least
+    /// three vertices, the given vertices are coplanar, and indices exist for
+    /// all vertices, `None` otherwise.
+    pub fn new(vertices: Vec<usize>, vertex_list: &[Vec3]) -> Option<Polygon> {
+        let normal = (vertex_list[vertices[1]] - vertex_list[vertices[0]])
+            .cross(vertex_list[vertices[2]] - vertex_list[vertices[0]])
+            .normalized();
+
+        todo!("Check if the vertices are coplanar.");
+        // Some(Polygon { vertices, normal })
+    }
+}
+
 /// A struct representing a mesh of triangles. Contains
 /// a `Vec<Triangle>` of the triangles in the mesh. The `TriMesh` also contains
 /// a `Vec<Vec3>` of the vertices in the mesh. The triangles contain indices
 /// into the `Vec<Vec3>` of the vertices.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct TriMesh {
     /// The list of triangles in the mesh.
     pub triangles: Vec<Triangle>,
@@ -84,7 +109,7 @@ pub struct TriMesh {
 /// a `Vec<Polygon>` of the polygons in the mesh. The `PolyMesh` also contains
 /// a `Vec<Vec3>` of the vertices in the mesh. The polygons contain indices
 /// into the `Vec<Vec3>` of the vertices.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct PolyMesh {
     /// The list of polygons in the mesh.
     pub polygons: Vec<Polygon>,
@@ -106,11 +131,8 @@ impl TriMesh {
     /// assert_eq!(mesh.triangles.len(), 0);
     /// assert_eq!(mesh.vertices.len(), 0);
     /// ```
-    pub fn new() -> TriMesh {
-        TriMesh {
-            triangles: Vec::new(),
-            vertices: Vec::new(),
-        }
+    pub fn new() -> Self {
+        Default::default()
     }
 
     /// Creates a new `TriMesh` from a list of vertices and a list of triangles.
@@ -140,7 +162,7 @@ impl TriMesh {
     ///   Triangle { vertices: [0, 1, 2], normal: Vec3::new(0.0, 0.0, 1.0) },
     /// ];
     ///
-    /// let mesh = TriMesh::from(vertices, triangles).unwrap(); // We know this is valid.
+    /// let mesh = TriMesh::from(&vertices, &triangles).unwrap(); // We know this is valid.
     ///
     /// assert_eq!(mesh.triangles.len(), 1);
     /// assert_eq!(mesh.vertices.len(), 3);
@@ -148,7 +170,7 @@ impl TriMesh {
     /// assert_eq!(mesh.triangles[0].vertices, [0, 1, 2]);
     /// assert_eq!(mesh.triangles[0].normal, Vec3::new(0.0, 0.0, 1.0));
     /// ```
-    pub fn from(vertices: Vec<Vec3>, triangles: Vec<Triangle>) -> Option<TriMesh> {
+    pub fn from(vertices: &[Vec3], triangles: &[Triangle]) -> Option<Self> {
         // Ensure that each triangle's vertex indices are valid. If any are
         // invalid, return None.
         if triangles
@@ -158,8 +180,8 @@ impl TriMesh {
             return None;
         }
         Some(TriMesh {
-            vertices,
-            triangles,
+            vertices: vertices.to_vec(),
+            triangles: triangles.to_vec(),
         })
     }
 
@@ -177,7 +199,7 @@ impl TriMesh {
     ///
     /// let mut mesh = TriMesh::new();
     /// mesh.add_triangle(
-    ///     [
+    ///     &[
     ///         Vec3::new(0.0, 0.0, 0.0),
     ///         Vec3::new(1.0, 0.0, 0.0),
     ///         Vec3::new(0.0, 1.0, 0.0)
@@ -193,7 +215,7 @@ impl TriMesh {
     /// ```
     ///
     /// This method has the danger of having duplicate vertices in the mesh.
-    pub fn add_triangle(&mut self, vertices: [Vec3; 3], normal: Vec3) {
+    pub fn add_triangle(&mut self, vertices: &[Vec3; 3], normal: Vec3) {
         self.triangles.push(Triangle {
             vertices: [
                 self.vertices.len(),
@@ -202,7 +224,7 @@ impl TriMesh {
             ],
             normal,
         });
-        self.vertices.extend_from_slice(&vertices);
+        self.vertices.extend_from_slice(vertices);
     }
 
     /// Deduplicates the vertices in the `TriMesh`. This method is in-place.
@@ -228,19 +250,19 @@ impl TriMesh {
     /// let mut mesh = TriMesh::new();
     ///
     /// mesh.add_triangle(
-    ///    [
-    ///    Vec3::new(0.0, 0.0, 0.0),
-    ///    Vec3::new(1.0, 0.0, 0.0),
-    ///    Vec3::new(0.0, 1.0, 0.0)
+    ///    &[
+    ///        Vec3::new(0.0, 0.0, 0.0),
+    ///        Vec3::new(1.0, 0.0, 0.0),
+    ///        Vec3::new(0.0, 1.0, 0.0)
     ///    ],
     ///    Vec3::new(0.0, 0.0, 1.0)
     /// );
     ///
     /// mesh.add_triangle(
-    ///   [
-    ///   Vec3::new(0.0, 0.0, 0.0),
-    ///   Vec3::new(1.0, 0.0, 0.0),
-    ///   Vec3::new(0.0, 1.0, 0.0)
+    ///   &[
+    ///       Vec3::new(0.0, 0.0, 0.0),
+    ///       Vec3::new(1.0, 0.0, 0.0),
+    ///       Vec3::new(0.0, 1.0, 0.0)
     ///   ],
     ///   Vec3::new(0.0, 0.0, 1.0)
     /// );
@@ -302,6 +324,58 @@ impl TriMesh {
         self.triangles = new_triangles;
     }
 
+    /// Remove unused vertices from the `TriMesh`. This method is in-place.
+    /// This method is useful for meshes that have many hanging vertices. This
+    /// method removes all vertices that are not referenced by any triangle.
+    ///
+    /// # Examples
+    /// ```
+    /// use phys_engine::entity::polymesh::{TriMesh, Triangle};
+    /// use ultraviolet::Vec3;
+    ///
+    /// let vertex_list = vec![
+    ///     Vec3::new(0.0, 0.0, 0.0),
+    ///     Vec3::new(5.0, 0.0, 0.0),
+    ///     Vec3::new(1.0, 0.0, 0.0),
+    ///     Vec3::new(0.0, 1.0, 0.0)
+    /// ];
+
+    /// let mut mesh = TriMesh::from(
+    ///     &vertex_list,
+    ///     &[Triangle::new([0, 2, 3], &vertex_list)]
+    /// ).unwrap();
+    ///
+    /// mesh.remove_unused_vertices();
+    ///
+    /// assert_eq!(mesh.vertices.len(), 3);
+    /// assert_eq!(mesh.triangles[0].vertices, [0, 1, 2]);
+    /// ```
+    pub fn remove_unused_vertices(&mut self) {
+        let mut new_vertices = Vec::new();
+        let mut new_triangles = Vec::new();
+
+        let mut indices: Vec<_> = self.triangles.iter().flat_map(|t| t.vertices).collect();
+        indices.sort();
+        indices.dedup();
+
+        indices.iter().for_each(|i| {
+            new_vertices.push(self.vertices[*i]);
+        });
+
+        self.triangles.iter().for_each(|t| {
+            new_triangles.push(
+                Triangle::new([
+                    indices.binary_search(&t.vertices[0]).unwrap(),
+                    indices.binary_search(&t.vertices[1]).unwrap(),
+                    indices.binary_search(&t.vertices[2]).unwrap(),
+                ], &new_vertices)
+            );
+        });
+
+        self.vertices = new_vertices;
+        self.triangles = new_triangles;
+    }
+
     /// Converts the `TriMesh` to a `PolyMesh`. This method will convert the
     /// triangles of the `TriMesh` into polygons with 3 vertices.
     ///
@@ -348,6 +422,83 @@ impl TriMesh {
     }
 }
 
+
+impl From<TriMesh> for PolyMesh {
+    fn from(mesh: TriMesh) -> Self {
+        mesh.into_poly_mesh()
+    }
+}
+
+impl PolyMesh {
+    /// Creates empty `PolyMesh` with no vertices or polygons.
+    ///
+    /// # Returns
+    /// An empty `PolyMesh`.
+    ///
+    /// # Examples
+    /// ```
+    /// use phys_engine::entity::polymesh::PolyMesh;
+    /// use ultraviolet::Vec3;
+    ///
+    /// let mesh = PolyMesh::new();
+    ///
+    /// assert_eq!(mesh.vertices.len(), 0);
+    /// assert_eq!(mesh.polygons.len(), 0);
+    /// ```
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Creates a `PolyMesh` from a list of vertices and a list of polygons.
+    ///
+    /// # Arguments
+    /// * `vertices` - A list of vertices.
+    /// * `polygons` - A list of polygons.
+    ///
+    /// # Returns
+    /// An `Option<PolyMesh>` containing `Some(PolyMesh)` if the vertices and
+    /// polygons are valid, `None` otherwise. The vertices and polygons are
+    /// valid if:
+    /// * Polygons do not contain any vertex that is not in the
+    /// vertices list.
+    /// * Polygons are coplanar.
+    ///
+    /// # Examples
+    /// ```
+    /// use phys_engine::entity::polymesh::{PolyMesh, Polygon};
+    /// use ultraviolet::Vec3;
+    ///
+    /// let vertices = vec![
+    ///    Vec3::new(0.0, 0.0, 0.0),
+    ///    Vec3::new(1.0, 0.0, 0.0),
+    ///    Vec3::new(1.0, 1.0, 0.0),
+    ///    Vec3::new(0.0, 1.0, 0.0)
+    /// ];
+    /// let polygons = vec![
+    ///   Polygon {
+    ///       vertices: vec![0, 1, 2, 3],
+    ///       normal: Vec3::new(0.0, 0.0, 1.0)
+    ///   }
+    /// ];
+    ///
+    /// let mesh = PolyMesh::from(&vertices, &polygons).unwrap();
+    ///
+    /// assert_eq!(mesh.vertices.len(), 4);
+    /// assert_eq!(mesh.polygons.len(), 1);
+    /// assert_eq!(mesh.polygons[0].vertices, vec![0, 1, 2, 3]);
+    /// ```
+    pub fn from(vertices: &[Vec3], polygons: &[Polygon]) -> Option<Self> {
+        if polygons.iter().flat_map(|f| &f.vertices).any(|&f| f >= vertices.len()) {
+            return None; // Invalid vertices
+        }
+        todo!("Check if polygons are coplanar");
+        // Some(PolyMesh {
+        //    vertices: vertices.to_vec(),
+        //    polygons: polygons.to_vec(),
+        // })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -360,7 +511,7 @@ mod tests {
         let mut mesh = TriMesh::new();
 
         mesh.add_triangle(
-           [
+           &[
            Vec3::new(0.0, 0.0, 0.0),
            Vec3::new(1.0, 0.0, 0.0),
            Vec3::new(0.0, 1.0, 0.0)
@@ -369,7 +520,7 @@ mod tests {
         );
 
         mesh.add_triangle(
-          [
+          &[
           Vec3::new(0.0, 0.0, 0.0),
           Vec3::new(1.0, 0.0, 0.0),
           Vec3::new(0.0, 1.0, 0.0)
