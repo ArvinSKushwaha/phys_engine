3b2b1ec1e7c430a715ac2e646830fc0154e16065
Arvin Singh Kushwaha <askushwa@ncsu.edu>
2022-04-28 02:02:01 -0400

    Added errors enums, geometric utilities, and GitHub Actions

--- src/entity/polymesh.rs
+++ src/entity/polymesh.rs
@@ -20,8 +20,50 @@
 //! method. This method will construct a new `PolyMesh` with the same vertex data as
 //! the `TriMesh` while converting the triangles of the `TriMesh` into faces with
 //! 3 vertices.
+//!
+//! TODO: Implement `flip_normal` for `PolyMesh` and `TriMesh` types.
 
+use crate::errors::PhysEngineErrors;
 use ultraviolet::Vec3;
+
+/// Computes the normal vector to 3 vertices in counter-clockwise order.
+///
+/// # Arguments
+/// * `v0` - The first vertex of the triangle.
+/// * `v1` - The second vertex of the triangle.
+/// * `v2` - The third vertex of the triangle.
+///
+/// # Returns
+/// A `Vec3` object representing the normal vector to the vertices. This function
+/// uses the right-hand rule.
+///
+pub(crate) fn get_normal(v0: Vec3, v1: Vec3, v2: Vec3) -> Vec3 {
+    let v1 = v1 - v0;
+    let v2 = v2 - v0;
+    v1.cross(v2)
+}
+
+/// Checks if vertices are coplanar. This is done by checking that each cross product
+/// points in the same direction.
+///
+/// # Arguments
+/// * `vertices` - A slice of `Vec3` points to check for coplanarity.
+///
+/// # Returns
+/// `true` if the vertices are coplanar, otherwise `false`.
+pub(crate) fn is_coplanar(vertices: &[Vec3]) -> bool {
+    const COPLANARITY_TOLERANCE: f32 = 0.00001;
+    // Naive Algorithm, simply check that all cross products are collinear.
+    // Should be O(n) which is fast, but also, makes me sad.
+    let normal = get_normal(vertices[0], vertices[1], vertices[2]);
+    for window in vertices.windows(3) {
+        let normal2 = get_normal(window[0], window[1], window[2]);
+        if normal2.cross(normal).mag_sq() > COPLANARITY_TOLERANCE {
+            return false;
+        }
+    }
+    true
+}
 
 /// A struct representing a `Triangle` in 3D space.
 /// The `Triangle` contains the indices of the three vertices that make up the
@@ -46,11 +88,17 @@ impl Triangle {
     ///
     /// # Returns
     /// A new `Triangle` with the given vertices.
-    pub fn new(vertices: [usize; 3], vertex_list: &[Vec3]) -> Triangle {
-        let normal = (vertex_list[vertices[1]] - vertex_list[vertices[0]])
-            .cross(vertex_list[vertices[2]] - vertex_list[vertices[0]])
-            .normalized();
-        Triangle { vertices, normal }
+    pub fn new(vertices: &[usize], vertex_list: &[Vec3]) -> Result<Triangle, PhysEngineErrors> {
+        if vertices.len() != 3 {
+            return Err(PhysEngineErrors::InvalidInputSize);
+        }
+        let vertices = [vertices[0], vertices[1], vertices[2]];
+        let normal = get_normal(
+            vertex_list[vertices[0]],
+            vertex_list[vertices[1]],
+            vertex_list[vertices[2]],
+        );
+        Ok(Triangle { vertices, normal })
     }
 }
 
@@ -88,8 +136,13 @@ impl Polygon {
             .cross(vertex_list[vertices[2]] - vertex_list[vertices[0]])
             .normalized();
 
-        todo!("Check if the vertices are coplanar.");
-        // Some(Polygon { vertices, normal })
+        if vertices.len() < 3
+            || !is_coplanar(&vertices.iter().map(|&i| vertex_list[i]).collect::<Vec<_>>())
+        {
+            return None;
+        }
+
+        Some(Polygon { vertices, normal })
     }
 }
 
@@ -97,6 +150,11 @@ impl Polygon {
 /// a `Vec<Triangle>` of the triangles in the mesh. The `TriMesh` also contains
 /// a `Vec<Vec3>` of the vertices in the mesh. The triangles contain indices
 /// into the `Vec<Vec3>` of the vertices.
+///
+/// The triangles comprising the mesh are expected to have the vertices in
+/// counter-clockwise order. This is how the normal is calculated. If this
+/// is not the case, the normal can be flipped by using the `flip_normal`
+/// method.
 #[derive(Debug, Clone, Default)]
 pub struct TriMesh {
     /// The list of triangles in the mesh.
@@ -109,6 +167,17 @@ pub struct TriMesh {
 /// a `Vec<Polygon>` of the polygons in the mesh. The `PolyMesh` also contains
 /// a `Vec<Vec3>` of the vertices in the mesh. The polygons contain indices
 /// into the `Vec<Vec3>` of the vertices.
+///
+/// The polygons must follow the following rules:
+/// * The vertices must be coplanar.
+/// * There must be at least 3 vertices.
+///
+/// It is recommended that the polygons are convex. This is not enforced. If
+/// polygon is convex, the normal will be calculated from the vertices by
+/// assuming the vertices are in counter-clockwise order and applying the
+/// right-hand rule. If not convex, the normal will be guessed from the first
+/// three vertices. These normals can be incorrect, but flippable using the
+/// `flip_normal` method.
 #[derive(Debug, Clone, Default)]
 pub struct PolyMesh {
     /// The list of polygons in the mesh.
@@ -278,14 +347,10 @@ impl TriMesh {
         let mut new_vertices = Vec::new();
         let mut new_triangles = Vec::new();
 
-        let compare_vertices = |a: &Vec3, b: &Vec3| {
-            (*a - *b).mag_sq() < max_dist * max_dist
-        };
-
-        use std::collections::HashMap;
+        let compare_vertices = |a: &Vec3, b: &Vec3| (*a - *b).mag_sq() < max_dist * max_dist;
 
         // Create a map of old vertex indices to their new indices.
-        let mut vertex_map = HashMap::new();
+        let mut vertex_map = Vec::new();
 
         // Iterate over all the vertices in the mesh and all vertices in new_vertices.
         // Using `compare_vertices` as the comparison function,
@@ -295,15 +360,14 @@ impl TriMesh {
         //
         // Note: This is a quadratic algorithm, if any better algorithm is
         // available, it should be implemented here.
-        // TODO: Implement a better algorithm.
 
-        self.vertices.iter().enumerate().for_each(|(i, v)| {
+        self.vertices.iter().for_each(|v| {
             match new_vertices.iter().position(|nv| compare_vertices(v, nv)) {
                 Some(j) => {
-                    vertex_map.insert(i, j);
+                    vertex_map.push(j);
                 }
                 None => {
-                    vertex_map.insert(i, new_vertices.len());
+                    vertex_map.push(new_vertices.len());
                     new_vertices.push(*v);
                 }
             }
@@ -312,9 +376,9 @@ impl TriMesh {
         self.triangles.iter().for_each(|t| {
             new_triangles.push(Triangle {
                 vertices: [
-                    vertex_map[&t.vertices[0]],
-                    vertex_map[&t.vertices[1]],
-                    vertex_map[&t.vertices[2]],
+                    vertex_map[t.vertices[0]],
+                    vertex_map[t.vertices[1]],
+                    vertex_map[t.vertices[2]],
                 ],
                 normal: t.normal,
             });
@@ -339,10 +403,10 @@ impl TriMesh {
     ///     Vec3::new(1.0, 0.0, 0.0),
     ///     Vec3::new(0.0, 1.0, 0.0)
     /// ];
-
+    ///
     /// let mut mesh = TriMesh::from(
     ///     &vertex_list,
-    ///     &[Triangle::new([0, 2, 3], &vertex_list)]
+    ///     &[Triangle::new(&[0, 2, 3], &vertex_list).unwrap()]
     /// ).unwrap();
     ///
     /// mesh.remove_unused_vertices();
@@ -364,11 +428,15 @@ impl TriMesh {
 
         self.triangles.iter().for_each(|t| {
             new_triangles.push(
-                Triangle::new([
-                    indices.binary_search(&t.vertices[0]).unwrap(),
-                    indices.binary_search(&t.vertices[1]).unwrap(),
-                    indices.binary_search(&t.vertices[2]).unwrap(),
-                ], &new_vertices)
+                Triangle::new(
+                    &[
+                        indices.binary_search(&t.vertices[0]).unwrap(),
+                        indices.binary_search(&t.vertices[1]).unwrap(),
+                        indices.binary_search(&t.vertices[2]).unwrap(),
+                    ],
+                    &new_vertices,
+                )
+                .unwrap(),
             );
         });
 
@@ -422,7 +490,6 @@ impl TriMesh {
     }
 }
 
-
 impl From<TriMesh> for PolyMesh {
     fn from(mesh: TriMesh) -> Self {
         mesh.into_poly_mesh()
@@ -475,10 +542,7 @@ impl PolyMesh {
     ///    Vec3::new(0.0, 1.0, 0.0)
     /// ];
     /// let polygons = vec![
-    ///   Polygon {
-    ///       vertices: vec![0, 1, 2, 3],
-    ///       normal: Vec3::new(0.0, 0.0, 1.0)
-    ///   }
+    ///   Polygon::new(vec![0, 1, 2, 3], &vertices).unwrap()
     /// ];
     ///
     /// let mesh = PolyMesh::from(&vertices, &polygons).unwrap();
@@ -488,14 +552,29 @@ impl PolyMesh {
     /// assert_eq!(mesh.polygons[0].vertices, vec![0, 1, 2, 3]);
     /// ```
     pub fn from(vertices: &[Vec3], polygons: &[Polygon]) -> Option<Self> {
-        if polygons.iter().flat_map(|f| &f.vertices).any(|&f| f >= vertices.len()) {
+        if polygons
+            .iter()
+            .flat_map(|f| &f.vertices)
+            .any(|&f| f >= vertices.len())
+        {
             return None; // Invalid vertices
         }
-        todo!("Check if polygons are coplanar");
-        // Some(PolyMesh {
-        //    vertices: vertices.to_vec(),
-        //    polygons: polygons.to_vec(),
-        // })
+        for polygon in polygons {
+            if !is_coplanar(
+                &polygon
+                    .vertices
+                    .iter()
+                    .map(|&f| vertices[f])
+                    .collect::<Vec<_>>(),
+            ) || polygon.vertices.len() < 3
+            {
+                return None; // Not coplanar or too few vertices
+            }
+        }
+        Some(Self {
+            vertices: vertices.to_vec(),
+            polygons: polygons.to_vec(),
+        })
     }
 }
 
@@ -511,21 +590,21 @@ mod tests {
         let mut mesh = TriMesh::new();
 
         mesh.add_triangle(
-           &[
-           Vec3::new(0.0, 0.0, 0.0),
-           Vec3::new(1.0, 0.0, 0.0),
-           Vec3::new(0.0, 1.0, 0.0)
-           ],
-           Vec3::new(0.0, 0.0, 1.0)
+            &[
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(1.0, 0.0, 0.0),
+                Vec3::new(0.0, 1.0, 0.0),
+            ],
+            Vec3::new(0.0, 0.0, 1.0),
         );
 
         mesh.add_triangle(
-          &[
-          Vec3::new(0.0, 0.0, 0.0),
-          Vec3::new(1.0, 0.0, 0.0),
-          Vec3::new(0.0, 1.0, 0.0)
-          ],
-          Vec3::new(0.0, 0.0, 1.0)
+            &[
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(1.0, 0.0, 0.0),
+                Vec3::new(0.0, 1.0, 0.0),
+            ],
+            Vec3::new(0.0, 0.0, 1.0),
         );
 
         mesh.dedup_vertices(epsilon);
@@ -534,5 +613,33 @@ mod tests {
 
         assert_eq!(mesh.triangles[0].vertices, [0, 1, 2]);
         assert_eq!(mesh.triangles[1].vertices, [0, 1, 2]);
+    }
+
+    #[test]
+    fn test_get_normals() {
+        use super::get_normal;
+        use ultraviolet::Vec3;
+
+        let v0 = Vec3::new(0.0, 0.0, 0.0);
+        let v1 = Vec3::new(1.0, 0.0, 0.0);
+        let v2 = Vec3::new(0.0, 1.0, 0.0);
+        let normal = get_normal(v0, v1, v2);
+
+        assert_eq!(normal, Vec3::new(0.0, 0.0, 1.0));
+    }
+
+    #[test]
+    fn test_is_coplanar() {
+        use super::is_coplanar;
+        use ultraviolet::Vec3;
+
+        let vertices = vec![
+            Vec3::new(0.0, 0.0, 0.0),
+            Vec3::new(1.0, 0.0, 0.0),
+            Vec3::new(1.0, 1.0, 0.0),
+            Vec3::new(0.0, 1.0, 0.0),
+        ];
+
+        assert!(is_coplanar(&vertices));
     }
 }
